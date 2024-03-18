46a567fb6fa7fadb69af40368c96437b0621b9ee
Niukalo Tetiana <tanusha.17121994@gmail.com>
2024-03-18 10:13:53 +0200

    add logic controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,62 @@
-import contactsService from "../services/contactsServices.js";
+import { HttpError } from "../helpers/HttpError.js";
+import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContactsById,
+} from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const result = await listContacts();
 
-export const getOneContact = (req, res) => {};
+  res.json(result);
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await getContactById(id);
 
-export const createContact = (req, res) => {};
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
 
-export const updateContact = (req, res) => {};
+  res.json(result);
+};
+
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await removeContact(id);
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+
+  res.json(result);
+};
+
+export const createContact = async (req, res) => {
+  const result = await addContact(req.body);
+
+  res.status(201).json(result);
+};
+
+export const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await updateContactsById(id, req.body);
+
+  if (!result) {
+    throw HttpError(400, "Body must have at least one field");
+  }
+
+  res.json(result);
+};
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+};
